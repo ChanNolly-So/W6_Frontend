d4de42a30df2a186a695bcd81b3491e5d276ecf2
ChanNolly-So <Channolly.so@student.cadt.edu.kh>
2026-02-26 15:17:18 +0700

    fix step2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 
 import Header from "./components/Header";
 import Scores from "./components/Scores"
-import { HTML_RESULTS, JAVA_RESULTS,PYTHON_RESULTS, ENGLISH_RESULTS } from "./data";
+import { HTML_RESULTS} from "./data";
 
 function App() {
   return (
     <>
        <Header batchName="Fake Batch"/>
       <main className="scores-container">
-        <Scores courseName="JAVA" courseResults={JAVA_RESULTS}/>
         <Scores courseName="HTML" courseResults={HTML_RESULTS}/>
-        <Scores courseName="PYTHON" courseResults={PYTHON_RESULTS}/>
-        <Scores courseName="ENGLISH" courseResults={ENGLISH_RESULTS}/>
+       
       </main>
     </>
   );
