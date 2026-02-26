dab94dd16431801eb56b1d44e53d3dde7365f3b3
ChanNolly-So <Channolly.so@student.cadt.edu.kh>
2026-02-26 16:11:14 +0700

    step5

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import Header from "./components/Header";
 import Scores from "./components/Scores"
-import { HTML_RESULTS} from "./data";
+import { HTML_RESULTS, JAVA_RESULTS, PYTHON_RESULTS, ENGLISH_RESULTS } from "./data";
 import Statistic from "./components/Statistic";
 
 function App() {
@@ -10,7 +10,9 @@ function App() {
        <Header batchName="Fake Batch"/>
       <main className="scores-container">
         <Scores courseName="HTML" courseResults={HTML_RESULTS}/>
-       
+        <Scores courseName="JAVA" courseResults={JAVA_RESULTS}/>
+        <Scores courseName="PYTHON" courseResults={PYTHON_RESULTS}/>
+        <Scores courseName="ENGLISH" courseResults={ENGLISH_RESULTS}/>
       </main>
     </>
   );
